06380c82e6addd9d77ee5b8514a5c9a9b1d47474
aayush017 <ayushmishra086@gmail.com>
2023-12-08 02:59:57 +0530

    mouse events

---
+++ src/main/java/com/example/interstellatstickhero/GameController.java
@@ -0,0 +1,48 @@
+package com.example.interstellatstickhero;
+
+import javafx.scene.input.MouseEvent;
+
+public class GameController {
+
+    private final StickHero stickHero;
+    private boolean isIncreasing;
+
+    public GameController(StickHero stickHero) {
+        this.stickHero = stickHero;
+    }
+
+    public void handleMouseClicked(MouseEvent event) {
+        stickHero.setFlipped(!stickHero.isFlipped());
+    }
+
+    public void handleMousePressed(MouseEvent event) {
+        if (stickHero.isMoving()) {
+            return;
+        } else {
+            isIncreasing = true;
+
+            // Start a thread to increase the stick length continuously
+            Thread increaseStickThread = new Thread(() -> {
+                while (isIncreasing) {
+                    increaseStickLength();
+                    try {
+                        Thread.sleep(50); // Adjust the delay based on your needs
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+            increaseStickThread.start();
+        }
+    }
+
+    public void handleMouseReleased(MouseEvent event) {
+        isIncreasing = false;
+    }
+
+    private void increaseStickLength() {
+        // Implement the logic to increase the stick length
+        stickHero.increaseStickLength(5); // You can adjust the amount to increase
+    }
+}
